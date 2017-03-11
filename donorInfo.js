02ee89251c15bb611c860590473785b9303cd957
Anessa Ortner <anessa.ortner@gmail.com>
2017-03-11 12:37:07 -0600

    woring iife

--- donorInfo.js
+++ donorInfo.js
@@ -1,19 +1,35 @@
 var DonorInfo = (function(){
 	var donors = [
 		{
-			name: "paula",
-			pledge: 80
+			name: "",
+			pledge: ''
 		}
 	]
+	var amountType = '';
 
 	return {
 		getDonor: function(){
-			console.log("getDonors works: ", donors);
+			if (donors.name !== undefined && donors.pledge !== undefined) {
+				ticketName.innerHTML = donors.name;
+				ticketAmount.innerHTML = "$" + donors.pledge;
+				ticketAmountType.innerHTML = amountType;
+			}
+
 			return donors;
 		},
 		addDonor: function(info){
-			console.log("addDonor works: ", info);
-		}
+			for (var x = 0; x < info.length; x++) {
+				if (info[x].id === "name" && info[x].value !== '') {
+					donors.name = info[x].value;
+				} if(info[x].id === "amount" && info[x].value !== ''){
+					donors.pledge = info[x].value;
+				} if(info[x].checked === true){
+					amountType = info[x].value;
+				} else if(info[x].value === ''){
+					warning.innerHTML = `<div class="warning">Please fill in all fields</div>`
+				}
+			}
 
+		}
 	}
 })();
--- main.js
+++ main.js
@@ -1,4 +1,28 @@
-console.log("main connected");
-console.log("DonorInfo object: ", DonorInfo);
-console.log("DonorInfo getDonor from main: ", DonorInfo.getDonor());
-console.log("DonorInfo addDonor from main: ", DonorInfo.addDonor("wazzzuuup from main"));
+var donateButton = document.getElementById("donate");
+var cancelButton = document.getElementById("cancel");
+
+var ticketName = document.getElementById("ticketName");
+var ticketAmount = document.getElementById("ticketAmount");
+var ticketAmountType = document.getElementById("ticketAmountType");
+
+function donate(){
+	var info = document.getElementsByTagName("input");
+	var warning = document.getElementById("warning");
+	warning.innerHTML = "";
+	DonorInfo.addDonor(info);
+	DonorInfo.getDonor();
+	donateButton.blur();
+}
+function clear (){
+	ticketName.innerHTML = "...";
+	ticketAmount.innerHTML = "...";
+	ticketAmountType.innerHTML = "...";
+	var info = document.getElementsByTagName("input");
+	for (var y = 0; y < info.length; y++) {
+		info[y].value = '';
+	}
+
+}
+
+donateButton.addEventListener("click", donate);
+cancelButton.addEventListener("click", clear);
