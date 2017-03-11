b6f8832e8ca28162335df97e9eadffffa1daaeab
Anessa Ortner <anessa.ortner@gmail.com>
2017-03-11 15:24:17 -0600

    fixed display of saved donations

--- donorInfo.js
+++ donorInfo.js
@@ -1,35 +1,42 @@
 var DonorInfo = (function(){
-	var donors = [
-		{
-			name: "",
-			pledge: ''
-		}
-	]
+	var donors = [];
 	var amountType = '';
 
 	return {
 		getDonor: function(){
-			if (donors.name !== undefined && donors.pledge !== undefined) {
-				ticketName.innerHTML = donors.name;
-				ticketAmount.innerHTML = "$" + donors.pledge;
-				ticketAmountType.innerHTML = amountType;
+			var ticketTable = document.getElementById("ticketTable");
+			var ticketString = '';
+			ticketString += `<tbody><th>Name:</th><th>Pledge:</th><th>Pledge Type:</th>`
+			for (var a = 0; a < donors.length; a++){
+			if (true) {
+				
+				ticketString += `<tr><td id="ticketName">${donors[a].name}</td>`
+				ticketString += `<td id="ticketName">${donors[a].pledge}</td>`
+				ticketString += `<td id="ticketName">${donors[a].amountType}</td></tr>`  
+				}
 			}
+			ticketString += `</tbody>`
+			ticketTable.innerHTML = ticketString;
+			console.log(ticketString);
 
 			return donors;
 		},
-		addDonor: function(info){
+		addDonor: function(){
+				donors.push({name: "", pledge:"", amountType:""});
+		},
+		setDonor: function(info){
+			var donorsIndex = donors.length - 1;
 			for (var x = 0; x < info.length; x++) {
 				if (info[x].id === "name" && info[x].value !== '') {
-					donors.name = info[x].value;
+					donors[donorsIndex].name = info[x].value;
 				} if(info[x].id === "amount" && info[x].value !== ''){
-					donors.pledge = info[x].value;
+					donors[donorsIndex].pledge = info[x].value;
 				} if(info[x].checked === true){
-					amountType = info[x].value;
+					donors[donorsIndex].amountType = info[x].value;
 				} else if(info[x].value === ''){
 					warning.innerHTML = `<div class="warning">Please fill in all fields</div>`
 				}
 			}
-
 		}
 	}
 })();
--- main.js
+++ main.js
@@ -1,22 +1,18 @@
 var donateButton = document.getElementById("donate");
 var cancelButton = document.getElementById("cancel");
 
-var ticketName = document.getElementById("ticketName");
-var ticketAmount = document.getElementById("ticketAmount");
-var ticketAmountType = document.getElementById("ticketAmountType");
-
 function donate(){
 	var info = document.getElementsByTagName("input");
 	var warning = document.getElementById("warning");
 	warning.innerHTML = "";
-	DonorInfo.addDonor(info);
-	DonorInfo.getDonor();
-	donateButton.blur();
+	if (info[0].value !== '' && info[1].value !== ''){
+		DonorInfo.addDonor();
+		DonorInfo.setDonor(info);
+		DonorInfo.getDonor();
+		donateButton.blur();
+	}
 }
 function clear (){
-	ticketName.innerHTML = "...";
-	ticketAmount.innerHTML = "...";
-	ticketAmountType.innerHTML = "...";
 	var info = document.getElementsByTagName("input");
 	for (var y = 0; y < info.length; y++) {
 		info[y].value = '';
